05ffb22181dba9f7f91bcf4786c2700b9ce61dcd
Kinga Paszkiewicz <kingaa.kulig@gmail.com>
2021-01-27 12:07:44 +0100

    Próba spushowania na odpowiednie repozytorium

--- src/Array.java
+++ src/Array.java
@@ -9,13 +9,9 @@ public class Array {
         bubbleSort.bubbleSort(array1);
         System.out.println("Tablica ma teraz postać: " + Arrays.toString(array1));
 
-
         System.out.println("Tabica numer 2:");
         int[] array2 = {5, 4, 3, 2, 1};
         bubbleSort.bubbleSort(array2);
-    }
-}
-
 
 //        System.out.println("Tabica numer 3:");
 //        int[] array3 = new int[100];
@@ -26,12 +22,13 @@ public class Array {
 //            System.out.println((array3[i]));
 //        }
 
-
-//        }
 //        System.out.println("Tabica numer 4:");
 //        int[] array4 = new int[100];
-//        for (int i = 99; i < array4.length; i--) {
-//            array4[i] = i;
-//            bubbleSort.bubbleSort(array4);
-//            System.out.println(Arrays.toString(array4));
+//        for (int i = 0; i < array4.length; i--) {
+//        array4[i] = i + 1;
+//        bubbleSort.bubbleSort(array4);
+//        System.out.println(Arrays.toString(array4));
+
+    }
+}
 
